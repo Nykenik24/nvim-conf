2a602538ad26fbe4e83a1a03645f89dcf19e0db9
Nykenik24 <Nykenik24@proton.me>
2025-02-06 18:04:24 +0100

    init.lua

---
+++ init.lua
@@ -0,0 +1,15 @@
+require("config.plugin.lazy")
+
+-- plugin configurations --
+
+-- nvim-notify
+require("config.plugin.notify")
+-- nvim-cmp
+require("config.plugin.cmp")
+-- lsp
+require("config.plugin.lsp")
+-- require configurations --
+require("config.user.keymap")
+require("config.user.autocmd")
+require("config.user.colorscheme")
+require("config.user.options")
