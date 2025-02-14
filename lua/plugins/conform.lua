5b3cd19a8bcc1890b0b1f08088515a038745be66
Nykenik24 <Nykenik24@proton.me>
2025-02-14 20:43:08 +0100

    Add some formatters

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,12 +5,14 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				-- Conform will run multiple formatters sequentially
-				python = { "pylint" },
+				python = { "pylint", "brunette", "isort" },
 				-- You can customize some of the format options for the filetype (:help conform.format)
 				rust = { "rustfmt", lsp_format = "fallback" },
 				-- Conform will run the first available formatter
 				javascript = { "prettierd", "prettier", stop_after_first = true },
 				sh = { "shfmt" },
+				css = { "prettied" },
+				html = { "htmlbeautifier" },
 			},
 		})
 	end,
